2b36455a0fe6fb2165b100d46653cd982832c653
Kirkleve <102994769+Kirkleve@users.noreply.github.com>
2022-06-14 23:25:49 +0200

    commplite Diagrams and Code, work whith text

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,19 +1,39 @@
-﻿/*
-Написать программу, которая из имеющегося массива строк формирует 
-массив из строк, длина которых меньше либо равна 3 символа. 
-Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
- При решении не рекомендуется пользоваться, коллекциями, лучше обойтись исключительно массивами.
+﻿int FindLenght (string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
 
-["hello","2","world",":-)"] -> ["2",":-)"]
-["1234","1567","-2","computer science"] -> ["-2"]
-["Russia","Denmark","Kazan"] -> []
-*/
+string[] CreateNewArray(string[] array, int lenght)
+{
+    string[] newArray = new string[lenght];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[count] += array[i];
+            count++;
+            if (count >= lenght)
+            {
+                break;
+            }
+        }
+    }
+    return newArray;
+}
+
 string PrintArray(string[] array)
 {
-    string restore = String.Empty;
-    int size = array.Length;
-    restore = "[";
-    for (int i = 0; i < size; i++)
+    string restore = "[";
+    for (int i = 0; i < array.Length; i++)
     {
         restore += $" {array[i]}";
     }
@@ -21,17 +41,8 @@ string PrintArray(string[] array)
     return restore;
 }
 
-string[] array = new string[] {"hello","2","world",":-)"};
+string[] array = new string[] { "hello", "2", "world", ":-)" };
+
 Console.WriteLine(PrintArray(array));
-
-
-for (int i =0; i< array.Length;i++)
-{
-    int rand = new Random().Next(array[i],array[i+1]);
-    if (array[i] == array[rand])
-    {
-
-    }
-}
-
+Console.WriteLine(PrintArray(CreateNewArray(array,FindLenght(array))));
 
